b247f638ff30714aaa1baf7864410abaa37fca73
danrenna <69612027+danrenna@users.noreply.github.com>
2020-08-15 17:11:47 -0400

    Adding required Heroku files to master

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,14 @@
 __author__ = 'jslvtr'
 
+import os
 
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
+# changing to use Heroku Env variables
+# will define these 3 environment variables inside Heroku
+# when this file opens, it will load env variables into 3 below variables
+# will be set in Heroku account, no one else will be able to see them in GitHub repo
+
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
